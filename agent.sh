fd4580e20efd7922911ba22df37f5cf3de700cdf
gatunosh <adrigato3@gmail.com>
2022-11-02 15:00:55 -0500

    [IMP] Doing the script more general

--- agent.sh
+++ agent.sh
@@ -3,9 +3,9 @@
 echo "Installing"
 echo "==========================================================="
 
-apt-get install jq
-apt-get net-tools
-apt-get install network-manager
+#apt-get install jq
+#apt-get net-tools
+#apt-get install network-manager
 
 echo "==========================================================="
 echo "Starting the script"
@@ -15,32 +15,38 @@ json="assets_values.json"
 
 lshw -json > $json
 
+json_len=( $(jq length $json) )
+if [ "$json_len" != 1 ] 
+then
+        json=( $(jq '.[]' $json) )
+        echo "${json[@]}" > assets_values.json
+fi
+
 echo -e '{' > assets.json
 
+PCModel=$(jq '.[] .product' $json)
+echo "\"PCModel\": $PCModel," >> assets.json
 
-PCModel=$(jq -r '.[] .product' $json)
-echo "\"PCModel\": \"$PCModel\"," >> assets.json
+PCManufacturer=$(jq '.[] .vendor' $json)
+echo "\"PCManufacturer\": $PCManufacturer," >> assets.json
 
-PCManufacturer=$(jq -r '.[] .vendor' $json)
-echo "\"PCManufacturer\": \"$PCManufacturer\"," >> assets.json
+Hostname=$(jq '.[] .id' $json)
+echo "\"Hostname\": $Hostname," >> assets.json
 
-Hostname=$(jq -r '.[] .id' $json)
-echo "\"Hostname\": \"$Hostname\"," >> assets.json
+ChassisType=$(jq '.[] .configuration .chassis' $json)
+echo "\"ChassisType\": $ChassisType," >> assets.json
 
-ChassisType=$(jq -r '.[] .configuration .chassis' $json)
-echo "\"ChassisType\": \"$ChassisType\"," >> assets.json
+WirelessAdapter=$(jq '.[] .children[0] .children[3] .children[8] .children[0] .product' $json)
+echo "\"WirelessAdapter\": $WirelessAdapter," >> assets.json
 
-WirelessAdapter=$(jq -r '.[] .children[0] .children[3] .children[8] .children[0] .product' $json)
-echo "\"WirelessAdapter\": \"$WirelessAdapter\"," >> assets.json
+WirelessMAC=$(jq '.[] .children[0] .children[3] .children[8] .children[0] .serial' $json)
+echo "\"WirelessMAC\": $WirelessMAC," >> assets.json
 
-WirelessMAC=$(jq -r '.[] .children[0] .children[3] .children[8] .children[0] .serial' $json)
-echo "\"WirelessMAC\": \"$WirelessMAC\"," >> assets.json
+WiredAdapter=$(jq '.[] .children[0] .children[3] .children[9] .children[0] .product' $json)
+echo "\"WiredAdapter\": $WiredAdapter," >> assets.json
 
-WiredAdapter=$(jq -r '.[] .children[0] .children[3] .children[9] .children[0] .product' $json)
-echo "\"WiredAdapter\": \"$WiredAdapter\"," >> assets.json
-
-WiredMAC=$(jq -r '.[] .children[0] .children[3] .children[9] .children[0] .serial' $json)
-echo "\"WiredMAC\": \"$WiredMAC\"," >> assets.json
+WiredMAC=$(jq '.[] .children[0] .children[3] .children[9] .children[0] .serial' $json)
+echo "\"WiredMAC\": $WiredMAC," >> assets.json
 
 video_controller_vendor=$(jq -r '.[] .children[0] .children[3] .children[0] .vendor' $json)
 video_controller_product=$(jq -r '.[] .children[0] .children[3] .children[0] .product' $json)
@@ -196,7 +202,7 @@ else
         echo "\"CurrentlyLoggedOn\": false," >> assets.json
 fi
 
-echo "\"Computer\": \"$Hostname\"" >> assets.json
+echo "\"Computer\": $Hostname" >> assets.json
 
 echo '},' >> assets.json
 
